cdc6bd4bca8be4894d71b722aeb02b07e1ae8301
Michael P. Daugherty <mike@mpdaugherty.com>
2012-12-22 00:12:14 +0800

    Initial Rails commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MagazineReader::Application.config.secret_token = '65098bdc93df3b0eea91a3074ad9da4fa8a03a476e8f5d09e502647779cf12c482a556151e7a898916a8c38b51dd1bca30035e3d50284890f65941000fe4578f'
