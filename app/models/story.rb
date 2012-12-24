94fd142f3dbd39e47d70c35ee58e42ab55717004
Michael P. Daugherty <mike@mpdaugherty.com>
2012-12-24 12:45:08 +0800

    Adding validation to Stories and some notes for Tim.

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,13 @@
 class Story < ActiveRecord::Base
   belongs_to :magazine
   attr_accessible :author, :link, :published_at, :text, :title, :magazine_id
+
+  # This line ensures that no one can create a new story without at least a link, a title, and the body text
+  validates_presence_of :title, :link, :text
+
+  # This line makes sure that links are valid URLs.  A URL is a special type of URI (Uniform Resource Identifier),
+  # and there is a Rails modules called URI as well.  We use its 'ABS_URI' method to match :link.  Absolute URIs have
+  # to include the transfer protocol (e.g. http:// or https://), the server address (e.g. www.mpdaugherty.com),
+  # and the file path (/something/something)
+  validates_format_of :link, :with => URI::ABS_URI, :message => 'is not a valid URL'
 end
