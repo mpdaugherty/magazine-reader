2d135b53ac3531133da902e21ce2b5d67652526a
Michael P. Daugherty <mike@mpdaugherty.com>
2012-12-28 14:21:58 +0800

    Added the Annotate gem

--- test/unit/story_test.rb
+++ test/unit/story_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: stories
+#
+#  id           :integer          not null, primary key
+#  title        :string(255)
+#  author       :string(255)
+#  link         :string(255)
+#  published_at :date
+#  text         :text
+#  magazine_id  :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 require 'test_helper'
 
 class StoryTest < ActiveSupport::TestCase
