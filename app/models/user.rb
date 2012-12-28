2bcef854aee032313df257e9478972fa91ab1852
Michael P. Daugherty <mike@mpdaugherty.com>
2012-12-28 16:30:24 +0800

    Improving testing coverage of user and implementing it.  Still has one failing test.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,8 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+
+  validates_presence_of :email, :name
+  validates_uniqueness_of :email
+  validates_format_of :email, :with => /^.+@.+\..+$/, :message => 'is not a valid email address'
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,5 +19,28 @@ describe User do
 
   it { @user.should respond_to(:name) }
   it { @user.should respond_to(:email) }
+  it { @user.should be_valid }
 
+  describe "When there is no name" do
+    before { @user.name = '' }
+    it { @user.should_not be_valid }
+  end
+
+  describe "When there is no email" do
+    before { @user.email = '' }
+    it { @user.should_not be_valid }
+  end
+
+  describe "When the email address is invalid" do
+    before { @user.email = 'invalid_email_address' }
+    it { @user.should_not be_valid }
+  end
+
+  describe "When the email address is not unique" do
+    it "should not allow saving of non-unique emails" do
+      expect {
+        user2 = User.create({name: "Example User 2", email: 'exuser@example.com'})
+      }.to raise_error
+    end
+  end
 end
